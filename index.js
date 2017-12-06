0beca2f77b12b11033cf122b5058b36881412cc8
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2017-12-06 23:32:47 +0100

    added node dash button library

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-vars = require('./vars');
+const vars = require('./vars');
 const TeleBot = require('telebot');
+const dash_button = require('node-dash-button');
 const bot = new TeleBot(vars.TELEGRAM_BOT_TOKEN);
-
+var dash = dash_button([vars.AMAZON_DASH_ALERT_MAC, vars.AMAZON_DASH_IOT_MAC], null, null, 'all'); //address from step above
+//FATTO FIN ORA : settare codici in un file esterno, integrare dash button, creare repo
 bot.start();
-//TODO   settare codici in un file esterno, integrare dash button, creare repo
-bot.sendMessage(vars.TELEGRAM_BOT_TOKEN, 'yeah');
+
+//bot.sendMessage(vars.TELEGRAM_BOT_TOKEN, 'yeah');
+
+dash.on("detected", function (dash_id){
+    if (dash_id === vars.AMAZON_DASH_ALERT_MAC){
+        console.log("omg nonna ha bisogno di aiuto");
+        bot.sendMessage(vars.TELEGRAM_BOT_TOKEN, 'Ho bisogno di aiuto! Chiamami');
+        // type http post call to localhost Home-Assistant here to buzzer rPi
+    } else if (dash_id === vars.AMAZON_DASH_IOT_MAC){
+        console.log("omg found the other! turn on the lights");
+        //type http post call to localhost Home-Assistant here to turn on lights
+    }
+});
 
 bot.on('/hello', (msg) => {
-    return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }! You are cool!`);
+    return bot.sendMessage(msg.from.id, `Ciao ${ msg.from.first_name }! Ti trovo bene oggi ! :)`);
+});
+
+//aggiungere evento al bot
+bot.on('/accendi_luci', (msg) => {
+    //http post call to localhost HA to turn on lights
+});
+
+//aggiungere evento al bot
+bot.on('/spegni_luci', (msg) => {
+    //http post call to localhost HA to turn on lights
 });
--- vars.js
+++ vars.js
@@ -1,9 +1,11 @@
 exports.TELEGRAM_BOT_TOKEN = "404750815:AAGC4D0mNnjzrNk5P2hSoDNXMR3YStvjzh4";
 exports.CHAT_ID = "21859514";
+exports.AMAZON_DASH_ALERT_MAC = "8f:3f:20:33:54:44";
+exports.AMAZON_DASH_IOT_MAC = "2e:3f:20:33:54:22";
+
 //other possible variables 
 /**
  * exports.NEST_USER_NAME = "email@email.com";
 exports.NEST_PASSWORD = "PASSWORD";
 exports.NEST_STRUCTURE_ID = "Structure ID"; //Found from Nest API
-exports.AMAZON_DASH_BUTTON_MAC = "Found from node-dash-button";
  */
