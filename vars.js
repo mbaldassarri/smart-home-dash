4aaf0788d562faff3a2d461124af6a59f725f501
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2017-12-08 16:45:07 +0100

    add iot dash button MAC

--- vars.js
+++ vars.js
@@ -1,11 +1,4 @@
 exports.TELEGRAM_BOT_TOKEN = "404750815:AAGC4D0mNnjzrNk5P2hSoDNXMR3YStvjzh4";
 exports.CHAT_ID = "21859514";
 exports.AMAZON_DASH_ALERT_MAC = "00:fc:8b:54:34:1c";
-exports.AMAZON_DASH_IOT_MAC = "2e:3f:20:33:54:22";
-
-//other possible variables 
-/**
- * exports.NEST_USER_NAME = "email@email.com";
-exports.NEST_PASSWORD = "PASSWORD";
-exports.NEST_STRUCTURE_ID = "Structure ID"; //Found from Nest API
- */
+exports.AMAZON_DASH_IOT_MAC = "54:60:09:4b:45:0c";
