bb4f8e27b42b2fd37907d00cdb51d394c8149c20
Marco Baldassarri <marcobaldassarri@mangomobi.com>
2017-12-08 17:51:32 +0100

    change random call

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const bot = new TeleBot(vars.TELEGRAM_BOT_TOKEN);
 var dash = dash_button([vars.AMAZON_DASH_ALERT_MAC, vars.AMAZON_DASH_IOT_MAC], null, null, 'all'); 
 bot.start();
 
-//bot.sendMessage(vars.TELEGRAM_BOT_TOKEN, 'yeah');
+const alertArray = [
+    "Ho bisogno di aiuto! Chiamami",
+    "Do si jita?? \u{1F621}",
+    "Vai sempre jirenne, do si rriata?! \u{1F624}",
+    "Vemme ad aiutà che non me tengo ritta!",
+    "Sfacciataccia, veni su! \u{1F612}",
+    "NNAMARIA !!! NNAMARI' !!! \u{1F60A}",
+    "Me juti?? Grazie cocca..."
+  ];
 
 dash.on("detected", function (dash_id){
     if (dash_id === vars.AMAZON_DASH_ALERT_MAC){
-        bot.sendMessage(vars.CHAT_ID, 'Ho bisogno di aiuto! Chiamami');
+        bot.sendMessage(vars.CHAT_ID, alertArray[Math.floor(Math.random() * alertArray.length)]);
         // type http post call to localhost Home-Assistant here to buzzer rPi
     } else if (dash_id === vars.AMAZON_DASH_IOT_MAC){
-        console.log("omg found the other! turn on the lights");
+        bot.sendMessage(vars.CHAT_ID, "iot");
         //type http post call to localhost Home-Assistant here to turn on lights
     }
 });
--- vars.js
+++ vars.js
@@ -1,4 +1,6 @@
 exports.TELEGRAM_BOT_TOKEN = "404750815:AAGC4D0mNnjzrNk5P2hSoDNXMR3YStvjzh4";
+//chatid anna 507451960 production
+//chatid marco 21859514 for testing
 exports.CHAT_ID = "21859514";
 exports.AMAZON_DASH_ALERT_MAC = "00:fc:8b:54:34:1c";
-exports.AMAZON_DASH_IOT_MAC = "54:60:09:4b:45:0c";
+exports.AMAZON_DASH_IOT_MAC = "ac:63:be:78:b1:2e";
